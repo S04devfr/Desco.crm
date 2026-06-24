21aee1fe7a7d54d190bb00b264a5bfe4773fe4d0
S04devfr <S04devfr@example.com>
2026-06-24 13:39:47 +0500

    fix: Railway deploy - remove prisma.config.ts and move prisma to dependencies

--- prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@prisma/config";
-
-export default defineConfig({
-  datasource: {
-    adapter: "sqlite", // yoki "postgresql", "mysql", "mongodb"
-    url: process.env.DATABASE_URL,
-  },
-});
